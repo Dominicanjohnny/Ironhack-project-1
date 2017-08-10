80f4987ec6b8281c047d3c40a4f9683b1576e33b
instructor <ta@Ariels-MacBook-Pro.local>
2017-08-09 21:56:58 -0400

    updated game

--- js/game.js
+++ js/game.js
@@ -4,8 +4,8 @@ var world = boxbox.createWorld(canvasElem);
 world.createEntity({
     name: "player",
     shape: "circle",
-    radius: 1,
-    image: "pig.png",
+    radius: 2,
+    image: "34127.png",
     imageStretchToFit: true,
     density: 4,
     x: 2,
@@ -20,13 +20,13 @@ world.createEntity({
 world.createEntity({
     name: "player2",
     shape: "circle",
-    radius: 1,
-    image: "pig.png",
+    radius: 2,
+    image: "charizard.png",
     imageStretchToFit: true,
     density: 4,
     x: 2,
     onKeyDown: function(e) {
-        this.applyImpulse(50, 20);
+        this.applyImpulse(200, 60);
     }
 });
 
@@ -35,27 +35,26 @@ world.createEntity({
     name: "ground",
     shape: "square",
     type: "static",
-
     color: "rgb(0,100,0)",
-    width: 20,
-    height: .5,
+    width: 30,
+    height: '1.0',
     y: 12
 });
-
 
 var block = {
     name: "block",
     shape: "square",
     color: "brown",
-    width: .5,
-    height: 4,
+    image: "36236.png",
+    width: '.5',
+    height: 5,
     onImpact: function(entity, force) {
         if (entity.name() === "player") {
             this.color("black");
         }
     }
-};
 
+};
 world.createEntity(block, {
     x: 15
 });
@@ -65,8 +64,21 @@ world.createEntity(block, {
 });
 
 world.createEntity(block, {
+   x: 19
+});
+
+world.createEntity(block, {
+   x: 21
+});
+world.createEntity(block, {
     x: 16,
     y: 1,
     width: 4,
-    height: .5
+    height: '.5',
 });
+ world.createEntity(block,{
+   x: 18,
+   y: 2,
+   width: 4,
+   height: '.5',
+ });

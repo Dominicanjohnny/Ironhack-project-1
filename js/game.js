c5ed88c9a9c8bc6af4bf31c2bdb9d67ac1c44b3c
Johnatan <johnatan@johnatans-MacBook-Air.local>
2017-08-18 23:46:56 -0400

    working on fire

--- js/game.js
+++ js/game.js
@@ -20,9 +20,8 @@ function pauseGame() {
 }
 
 function clearForce(){
-  player.clearForce();
+
   player2.clearForce();
-  player.clearVelocity();
   player2.clearVelocity();
 }
 function keyDown(e) {
@@ -35,7 +34,9 @@ function keyDown(e) {
      }
 
 }
+function fire(){
 
+}
 
 
 
@@ -43,32 +44,68 @@ function keyDown(e) {
 var canvasElem = document.getElementById("game");
 var world = boxbox.createWorld(canvasElem);
 
-var player = world.createEntity({
-    name: "player",
-    shape: "circle",
-    radius: 2,
-    image: "34127.png",
-    imageStretchToFit: true,
-    density: 4,
-    x: 4,
-    onKeyDown: function(e) {
-        if(gamePaused){
-          return;
-        }
-        this.applyImpulse(200, 60);
-    }
-});
+// // var player = world.createEntity({
+//     name: "player",
+//     shape: "circle",
+//     radius: 2,
+//     image: "34127.png",
+//     imageStretchToFit: true,
+//     density: 4,
+//     x: 4,
+//     onKeyDown: function(e) {
+//         if(gamePaused){
+//           return;
+//         }
+//         this.applyImpulse(200, 60);
+//     }
+// });
+
+// var player2[].x;
+// var player2[].y;
+//
+// var direction = player2;
+//   if (direction == 'right') {
+//     player2++;
+//   } else if (direction == 'left') {
+//     player2--;
+//   } else if ( direction == 'up') {
+//     player2--;
+//   } else if ( direction == 'down') {
+//     player2++;
+//   }
 
 
 
+var navKeys = {
+  left: 37,
+  up: 38,
+  right: 39,
+  down:  40,
+  fire: 32
+};
+var fire = {
+  name: "fire",
+  shape: "circle",
+  color: "red",
+  image: "fire_img.png",
 
+  radius: 2,
+  imageStretchToFit: false,
+  density: 1,
+  x: 1,
+  onImpact: function(entity, force) {
 
+      if (entity.name() !== "ground" && entity.name() !== "player2") {
+          this.destroy();
+      }
+  }
+};
 
 var player2 = world.createEntity({
     name: "player2",
     shape: "circle",
     radius: 2,
-    image: "charizard.png",
+    image: "mewtwo.png",
     imageStretchToFit: true,
     density: 4,
     x: 4,
@@ -76,9 +113,59 @@ var player2 = world.createEntity({
         if(gamePaused){
           return;
         }
-        this.applyImpulse(200, 60);
+        console.log('Key Press: '+e.keyCode);
+        switch(e.keyCode){
+          case navKeys.left:
+           // move left
+           //this.clearForce();
+           //this.clearVelocity();
+           this.applyImpulse(200, -145);
+           break;
+          case navKeys.up:
+          // move up
+          this.applyImpulse(200, 90);
+          break;
+          case navKeys.right:
+          // move right
+          this.applyImpulse(200, 145);
+          break;
+          case navKeys.down:
+          // move down
+          this.applyImpulse(200, -180);
+          break;
+          case navKeys.fire:
+          var fire = world.createEntity({
+            name: "fire",
+            shape: "circle",
+            color: "red",
+            image: "fire_img.png",
+
+            radius: 1,
+            imageStretchToFit: false,
+            density: 2,
+            x: 5,
+            y:17,
+            onImpact: function(entity, force) {
+                if (entity.name() !== "ground" && entity.name() !== "player2") {
+                    this.destroy();
+                }
+            }
+          }, {
+            x: 5,
+
+            width: '.5',
+            height: '.5'
+          });
+          fire.applyImpulse(300, 130);
+          default:
+           /// do default here
+           break;
+
+        }
+
     }
 });
+
 
 
 world.createEntity({
@@ -91,6 +178,8 @@ world.createEntity({
     y: 20
 });
 
+
+
 var block = {
     name: "block",
     shape: "square",
@@ -99,14 +188,10 @@ var block = {
     width: .5,
     height: 5,
     onImpact: function(entity, force) {
-      console.log('ENTITY:: '+JSON.stringify(entity));
-      console.log('FORCE:: '+JSON.stringify(force));
         if (entity.name() === "player" || entity.name() === "player2") {
-            player.destroy();
             player2.destroy();
         }
     }
-
 };
 var block1 = {
   name: "block1",
@@ -120,22 +205,27 @@ var block1 = {
       if (entity.name() !== "ground") {
           this.destroy();
       }
-
   }
-
-
 };
+
+world.createEntity(block1, {
+    x: 111
+});
+
+world.createEntity(block1, {
+    x: 13
+});
+
 world.createEntity(block1, {
     x: 15
-
 });
 
 world.createEntity(block1, {
-    x: 17
+   x: 17
 });
 
 world.createEntity(block1, {
-   x: 19
+    x: 19
 });
 
 world.createEntity(block1, {
